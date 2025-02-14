847106172fc6dc8ed320609ee9aa20ef59274e71
snmln <smalone@element84.com>
2025-02-14 16:42:35 -0500

    creating wind raster

--- src/App.js
+++ src/App.js
@@ -1,50 +1,115 @@
-import React, { useEffect, useRef } from 'react';
-import mapboxgl from 'mapbox-gl';
-import './App.css'
-import 'mapbox-gl/dist/mapbox-gl.css';
+import React, { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const MapboxExample = () => {
   const mapContainerRef = useRef();
   const mapRef = useRef();
 
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoic25tbG4iLCJhIjoiY202ZmdhcDZ0MDR3ZTJpcTB4dmtoZTF3aCJ9.D8agtdsV1t5f2AgjQgLM4w';
+    mapboxgl.accessToken =
+      "pk.eyJ1Ijoic25tbG4iLCJhIjoiY202ZmdhcDZ0MDR3ZTJpcTB4dmtoZTF3aCJ9.D8agtdsV1t5f2AgjQgLM4w";
 
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/light-v11',
-      zoom: 13,
-      center: [-122.447303, 37.753574]
+      maxZoom: 2,
+      minZoom: 2,
+      zoom: 2,
+      center: [-28, 47],
+      style: "mapbox://styles/mapbox/dark-v11",
     });
 
-    mapRef.current.on('load', () => {
-      mapRef.current.addSource('mapbox-terrain', {
-        type: 'vector',
-        url: 'mapbox://mapbox.mapbox-terrain-v2'
-      });
-      mapRef.current.addLayer(
-        {
-          id: 'terrain-data',
-          type: 'line',
-          source: 'mapbox-terrain',
-          'source-layer': 'contour',
-          layout: {
-            'line-join': 'round',
-            'line-cap': 'round'
-          },
+    mapRef.current.on("load", () => {
+      if (!mapRef.current.getSource("raster-array-source")) {
+        mapRef.current.addSource("raster-array-source", {
+          type: "raster-array",
+          url: "mapbox://rasterarrayexamples.gfs-winds",
+          tileSize: 512,
+        });
+      }
+      if (!mapRef.current.getLayer("wind-layer")) {
+        mapRef.current.addLayer({
+          id: "wind-layer",
+          type: "raster-particle",
+          source: "raster-array-source",
+          "source-layer": "10winds",
           paint: {
-            'line-color': '#ff69b4',
-            'line-width': 1
-          }
-        },
-        'road-label-simple'
-      );
+            "raster-particle-speed-factor": 0.4,
+            "raster-particle-fade-opacity-factor": 0.9,
+            "raster-particle-reset-rate-factor": 0.4,
+            "raster-particle-count": 4000,
+            "raster-particle-max-speed": 40,
+            "raster-particle-color": [
+              "interpolate",
+              ["linear"],
+              ["raster-particle-speed"],
+              1.5,
+              "rgba(134,163,171,256)",
+              2.5,
+              "rgba(126,152,188,256)",
+              4.12,
+              "rgba(110,143,208,256)",
+              4.63,
+              "rgba(110,143,208,256)",
+              6.17,
+              "rgba(15,147,167,256)",
+              7.72,
+              "rgba(15,147,167,256)",
+              9.26,
+              "rgba(57,163,57,256)",
+              10.29,
+              "rgba(57,163,57,256)",
+              11.83,
+              "rgba(194,134,62,256)",
+              13.37,
+              "rgba(194,134,63,256)",
+              14.92,
+              "rgba(200,66,13,256)",
+              16.46,
+              "rgba(200,66,13,256)",
+              18.0,
+              "rgba(210,0,50,256)",
+              20.06,
+              "rgba(215,0,50,256)",
+              21.6,
+              "rgba(175,80,136,256)",
+              23.66,
+              "rgba(175,80,136,256)",
+              25.21,
+              "rgba(117,74,147,256)",
+              27.78,
+              "rgba(117,74,147,256)",
+              29.32,
+              "rgba(68,105,141,256)",
+              31.89,
+              "rgba(68,105,141,256)",
+              33.44,
+              "rgba(194,251,119,256)",
+              42.18,
+              "rgba(194,251,119,256)",
+              43.72,
+              "rgba(241,255,109,256)",
+              48.87,
+              "rgba(241,255,109,256)",
+              50.41,
+              "rgba(256,256,256,256)",
+              57.61,
+              "rgba(256,256,256,256)",
+              59.16,
+              "rgba(0,256,256,256)",
+              68.93,
+              "rgba(0,256,256,256)",
+              69.44,
+              "rgba(256,37,256,256)",
+            ],
+          },
+        });
+      }
     });
-
-    return () => mapRef.current.remove();
   }, []);
 
-  return <div id="map" ref={mapContainerRef} ></div>;
+  return <div id="map" ref={mapContainerRef}></div>;
 };
 
 export default MapboxExample;
